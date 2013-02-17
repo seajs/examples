1d3cccdc301c214f4897726ec176de2c17bb42ed
lifesinger <lifesinger@gmail.com>
2013-02-17 08:56:27 +0800

    Update sea.js to 2.0.0b2

--- assets/seajs/sea-debug.js
+++ assets/seajs/sea-debug.js
@@ -1,5 +1,5 @@
 /**
- * @preserve SeaJS v2.0.0b1 | seajs.org/LICENSE.md
+ * @preserve SeaJS v2.0.0b2 | seajs.org/LICENSE.md
  */
 (function(global, undefined) {
 "use strict"
@@ -12,7 +12,7 @@ if (_seajs && !_seajs.args) {
 
 var seajs = global.seajs = {
   // The current version of SeaJS being used
-  version: "2.0.0b1"
+  version: "2.0.0b2"
 }
 
 
@@ -20,34 +20,15 @@ var seajs = global.seajs = {
  * util-lang.js - The minimal language enhancement
  */
 
-var hasOwnProperty = {}.hasOwnProperty
-
-function hasOwn(obj, prop) {
-  return hasOwnProperty.call(obj, prop)
-}
-
-function isFunction(obj) {
-  return typeof obj === "function"
-}
-
-var isArray = Array.isArray || function(obj) {
-  return obj instanceof Array
-}
-
-function unique(arr) {
-  var obj = {}
-  var ret = []
-
-  for (var i = 0, len = arr.length; i < len; i++) {
-    var item = arr[i]
-    if (obj[item] !== 1) {
-      obj[item] = 1
-      ret.push(item)
-    }
+function isType(type) {
+  return function(obj) {
+    return Object.prototype.toString.call(obj) === "[object " + type + "]"
   }
-
-  return ret
 }
+
+var isObject = isType("Object")
+var isArray = Array.isArray || isType("Array")
+var isFunction = isType("Function")
 
 
 /**
@@ -58,16 +39,12 @@ function unique(arr) {
 // log("message") ==> console.log("message")
 // log("message", "warn") ==> console.warn("message")
 var log = seajs.log = function(msg, type) {
-  var console = global.console
 
-  if (console) {
-    // Do NOT print `log(msg)` in non-debug mode
-    if (type || configData.debug) {
-      if (console[type || (type = "log")]) {
-        console[type](msg)
-      }
-    }
-  }
+  global.console &&
+      // Do NOT print `log(msg)` in non-debug mode
+      (type || configData.debug) &&
+      // Set the default value of type
+      (console[type || (type = "log")]) && console[type](msg)
 
 }
 
@@ -76,7 +53,7 @@ var log = seajs.log = function(msg, type) {
  * util-events.js - The minimal events support
  */
 
-var eventsCache = {}
+var eventsCache = seajs.events = {}
 
 // Bind event
 seajs.on = function(event, callback) {
@@ -94,7 +71,7 @@ seajs.on = function(event, callback) {
 seajs.off = function(event, callback) {
   // Remove *all* events
   if (!(event || callback)) {
-    eventsCache = {}
+    seajs.events = eventsCache = {}
     return seajs
   }
 
@@ -145,11 +122,6 @@ function emitData(event, data, prop) {
  */
 
 var DIRNAME_RE = /[^?]*(?=\/.*$)/
-var MULTIPLE_SLASH_RE = /([^:\/])\/\/+/g
-var URI_END_RE = /\?|\.(?:css|js)$|\/$/
-var ROOT_RE = /^(.*?:\/\/.*?)(?:\/|$)/
-var VARS_RE = /{([^{}]+)}/g
-
 
 // Extract the directory portion of a path
 // dirname("a/b/c.js") ==> "a/b/"
@@ -160,39 +132,35 @@ function dirname(path) {
   return (s ? s[0] : ".") + "/"
 }
 
+
+var DOT_RE = /\/\.\//g
+var DOUBLE_DOT_RE = /\/[^/]+\/\.\.\//g
+var MULTIPLE_SLASH_RE = /([^:\/])\/\/+/g
+
 // Canonicalize a path
 // realpath("http://test.com/a//./b/../c") ==> "http://test.com/a/c"
 function realpath(path) {
 
-  // "file:///a//b/c" ==> "file:///a/b/c"
-  // "http://a//b/c" ==> "http://a/b/c"
-  // "https://a//b/c" ==> "https://a/b/c"
-  if (path.lastIndexOf("//") > 7) {
-    path = path.replace(MULTIPLE_SLASH_RE, "$1\/")
+  // /a/b/./c/./d ==> /a/b/c/d
+  path = path.replace(DOT_RE, "/")
+
+  // "file:///a//b/c"  ==> "file:///a/b/c"
+  // "http://a//b/c"   ==> "http://a/b/c"
+  // "https://a//b/c"  ==> "https://a/b/c"
+  // "/a/b//"          ==> "/a/b/"
+  path = path.replace(MULTIPLE_SLASH_RE, "$1\/")
+
+  // a/b/c/../../d  ==>  a/b/../d  ==>  a/d
+  while (path.match(DOUBLE_DOT_RE)) {
+    path = path.replace(DOUBLE_DOT_RE, "/")
   }
 
-  // If "a/b/c", just return
-  if (path.indexOf(".") < 0) {
-    return path
-  }
-
-  var ret = []
-  var parts = path.split("/")
-  var part
-
-  for (var i = 0, len = parts.length; i < len; i++) {
-    part = parts[i]
-
-    if (part === "..") {
-      ret.pop()
-    }
-    else if (part !== ".") {
-      ret.push(part)
-    }
-  }
-
-  return ret.join("/")
+  return path
 }
+
+
+var URI_END_RE = /\?|\.(?:css|js)$|\/$/
+var HASH_END_RE = /#$/
 
 // Normalize an uri
 // normalize("path/to/a") ==> "path/to/a.js"
@@ -202,8 +170,7 @@ function normalize(uri) {
   uri = realpath(uri)
 
   // Add the default `.js` extension except that the uri ends with `#`
-  var lastChar = uri.charAt(uri.length - 1)
-  if (lastChar === "#") {
+  if (HASH_END_RE.test(uri)) {
     uri = uri.slice(0, -1)
   }
   else if (!URI_END_RE.test(uri)) {
@@ -217,11 +184,13 @@ function normalize(uri) {
 }
 
 
+var VARS_RE = /{([^{}]+)}/g
+
 function parseAlias(id) {
   var alias = configData.alias
 
   // Only parse top-level id
-  if (alias && hasOwn(alias, id) && isTopLevel(id)) {
+  if (alias && alias.hasOwnProperty(id) && isTopLevel(id)) {
     id = alias[id]
   }
 
@@ -233,47 +202,19 @@ function parseVars(id) {
 
   if (vars && id.indexOf("{") >= 0) {
     id = id.replace(VARS_RE, function(m, key) {
-      return hasOwn(vars, key) ? vars[key] : "{" + key + "}"
+      return vars.hasOwnProperty(key) ? vars[key] : m
     })
   }
 
   return id
 }
 
-function addBase(id, refUri) {
-  var ret
-
-  // absolute id
-  if (isAbsolute(id)) {
-    ret = id
-  }
-  // relative id
-  else if (isRelative(id)) {
-    // Convert "./a" to "a", to avoid unnecessary loop in realpath() call
-    if (id.indexOf("./") === 0) {
-      id = id.substring(2)
-    }
-    ret = dirname(refUri) + id
-  }
-  // root id
-  else if (isRoot(id)) {
-    ret = refUri.match(ROOT_RE)[1] + id
-  }
-  // top-level id
-  else {
-    ret = configData.base + id
-  }
-
-  return ret
-}
-
 function parseMap(uri) {
-  var map = configData.map || []
+  var map = configData.map
   var ret = uri
-  var len = map.length
 
-  if (len) {
-    for (var i = 0; i < len; i++) {
+  if (map) {
+    for (var i = 0; i < map.length; i++) {
       var rule = map[i]
 
       ret = isFunction(rule) ?
@@ -288,12 +229,39 @@ function parseMap(uri) {
   return ret
 }
 
+
+var ROOT_DIR_RE = /^(.*?:\/\/.*?)(?:\/|$)/
+
+function addBase(id, refUri) {
+  var ret
+
+  // absolute id
+  if (isAbsolute(id)) {
+    ret = id
+  }
+  // relative id
+  else if (isRelative(id)) {
+    ret = (refUri ? dirname(refUri) : cwd) + id
+  }
+  // root id
+  else if (isRoot(id)) {
+    var m = (refUri || cwd).match(ROOT_DIR_RE)
+    ret = (m ? m[1] : "") + id
+  }
+  // top-level id
+  else {
+    ret = configData.base + id
+  }
+
+  return ret
+}
+
 function id2Uri(id, refUri) {
   if (!id) return ""
 
   id = parseAlias(id)
   id = parseVars(id)
-  id = addBase(id, refUri || pageUri)
+  id = addBase(id, refUri)
   id = normalize(id)
   id = parseMap(id)
 
@@ -301,27 +269,31 @@ function id2Uri(id, refUri) {
 }
 
 
+var ABSOLUTE_RE = /(?:^|:)\/\/./
+var RELATIVE_RE = /^\.{1,2}\//
+var ROOT_RE = /^\//
+var TOPLEVEL_RE = /^[^./][^:]*$/
+
 function isAbsolute(id) {
-  return id.indexOf("://") > 0 || id.indexOf("//") === 0
+  return ABSOLUTE_RE.test(id)
 }
 
 function isRelative(id) {
-  return id.indexOf("./") === 0 || id.indexOf("../") === 0
+  return RELATIVE_RE.test(id)
 }
 
 function isRoot(id) {
-  return id.charAt(0) === "/" && id.charAt(1) !== "/"
+  return ROOT_RE.test(id)
 }
 
 function isTopLevel(id) {
-  var c = id.charAt(0)
-  return id.indexOf("://") < 0 && c !== "." && c !== "/"
+  return TOPLEVEL_RE.test(id)
 }
 
 
 var doc = document
 var loc = location
-var pageUri = loc.href.replace(/[?#].*$/, "")
+var cwd = dirname(loc.href)
 
 // Recommend to add `seajs-node` id for the `sea.js` script element
 var loaderScript = doc.getElementById("seajsnode") || (function() {
@@ -329,14 +301,19 @@ var loaderScript = doc.getElementById("seajsnode") || (function() {
   return scripts[scripts.length - 1]
 })()
 
-// When `sea.js` is inline, set loaderDir according to pageUri
-var loaderDir = dirname(getScriptAbsoluteSrc(loaderScript) || pageUri)
+// When `sea.js` is inline, set loaderDir to current working directory
+var loaderDir = dirname(getScriptAbsoluteSrc(loaderScript)) || cwd
 
 function getScriptAbsoluteSrc(node) {
   return node.hasAttribute ? // non-IE6/7
       node.src :
     // see http://msdn.microsoft.com/en-us/library/ms536429(VS.85).aspx
       node.getAttribute("src", 4)
+}
+
+// Get/set current working directory
+seajs.cwd = function(val) {
+  return val ? (cwd = realpath(val + "/")) : cwd
 }
 
 
@@ -501,15 +478,16 @@ var REQUIRE_RE = /"(?:\\"|[^"])*"|'(?:\\'|[^'])*'|\/\*[\S\s]*?\*\/|\/(?:\\\/|[^/
 var SLASH_RE = /\\\\/g
 
 function parseDependencies(code) {
-  var ret = [], m
-  REQUIRE_RE.lastIndex = 0
-  code = code.replace(SLASH_RE, "")
+  var ret = []
 
-  while ((m = REQUIRE_RE.exec(code))) {
-    if (m[2]) ret.push(m[2])
-  }
+  code.replace(SLASH_RE, "")
+      .replace(REQUIRE_RE, function(m, m1, m2) {
+        if (m2) {
+          ret.push(m2)
+        }
+      })
 
-  return unique(ret)
+  return ret
 }
 
 
@@ -543,7 +521,7 @@ Module.prototype.load = function(ids, callback) {
   load(uris, function() {
     var exports = []
 
-    for (var i = 0, len = uris.length; i < len; i++) {
+    for (var i = 0; i < uris.length; i++) {
       exports[i] = compile(cachedModules[uris[i]])
     }
 
@@ -551,15 +529,13 @@ Module.prototype.load = function(ids, callback) {
       callback.apply(global, exports)
     }
   })
-
-  return this
 }
 
 function resolve(ids, refUri) {
   if (isArray(ids)) {
     // Use `for` loop instead of `forEach` or `map` function for performance
     var ret = []
-    for (var i = 0, len = ids.length; i < len; i++) {
+    for (var i = 0; i < ids.length; i++) {
       ret[i] = resolve(ids[i], refUri)
     }
     return ret
@@ -848,7 +824,7 @@ function getModule(uri, status) {
 function getUnloadedUris(uris) {
   var ret = []
 
-  for (var i = 0, len = uris.length; i < len; i++) {
+  for (var i = 0; i < uris.length; i++) {
     var uri = uris[i]
     if (uri && getModule(uri).status < STATUS.LOADED) {
       ret.push(uri)
@@ -883,8 +859,14 @@ function isCircularWaiting(mod) {
 }
 
 function isOverlap(arrA, arrB) {
-  var arrC = arrA.concat(arrB)
-  return  unique(arrC).length < arrC.length
+  for (var i = 0; i < arrA.length; i++) {
+    for (var j = 0; j < arrB.length; j++) {
+      if (arrB[j] === arrA[i]) {
+        return true
+      }
+    }
+  }
+  return false
 }
 
 function cutWaitings(waitings) {
@@ -922,7 +904,7 @@ function preload(callback) {
 
 // Public API
 
-var globalModule = new Module(pageUri, STATUS.COMPILED)
+var globalModule = new Module(undefined, STATUS.COMPILED)
 
 seajs.use = function(ids, callback) {
   // Load preload modules before all other modules
@@ -969,42 +951,35 @@ var configData = config.data = {
 }
 
 function config(data) {
-  emit("config", data)
-
   for (var key in data) {
     var curr = data[key]
 
-    if (hasOwn(data, key) && curr !== undefined) {
-      // Convert plugins to preload config
-      if (key === "plugins") {
-        key = "preload"
-        curr = plugin2preload(curr)
+    // Convert plugins to preload config
+    if (curr && key === "plugins") {
+      key = "preload"
+      curr = plugin2preload(curr)
+    }
+
+    var prev = configData[key]
+
+    // Merge object config such as alias, vars
+    if (prev && isObject(prev)) {
+      for (var k in curr) {
+        prev[k] = curr[k]
+      }
+    }
+    else {
+      // Concat array config such as map, preload
+      if (isArray(prev)) {
+        curr = prev.concat(curr)
+      }
+      // Make sure that `configData.base` is an absolute directory
+      else if (key === "base") {
+        curr = normalize(addBase(curr + "/"))
       }
 
-      var prev = configData[key]
-
-      // For alias, vars
-      if (prev && /^(?:alias|vars)$/.test(key)) {
-        for (var k in curr) {
-          if (hasOwn(curr, k)) {
-            prev[k] = curr[k]
-          }
-        }
-      }
-      else {
-        // For map, preload
-        if (isArray(prev) && /^(?:map|preload)$/.test(key)) {
-          curr = prev.concat(curr)
-        }
-
-        // Set config
-        configData[key] = curr
-
-        // Make sure that `configData.base` is an absolute path
-        if (key === "base") {
-          makeBaseAbsolute()
-        }
-      }
+      // Set config
+      configData[key] = curr
     }
   }
 
@@ -1013,7 +988,6 @@ function config(data) {
 
 seajs.config = config
 
-
 function plugin2preload(arr) {
   var ret = [], name
 
@@ -1021,14 +995,6 @@ function plugin2preload(arr) {
     ret.push(loaderDir + "plugin-" + name)
   }
   return ret
-}
-
-function makeBaseAbsolute() {
-  var base = configData.base
-  if (!isAbsolute(base)) {
-    configData.base = id2Uri((isRoot(base) ? "" : "./") + base
-        + (base.charAt(base.length - 1) === "/" ? "" : "/"))
-  }
 }
 
 
