fdb9dfc3bae890a014ac04e3b48d98da45da8fee
lifesinger <lifesinger@gmail.com>
2013-03-05 10:38:38 +0800

    Update shim config

--- assets/hello/config.js
+++ assets/hello/config.js
@@ -1,16 +1,12 @@
 
 seajs.config({
-  // Set aliases for common libraries
-  alias: {
-    'jquery': 'common/jquery-1.9.1.min.js'
-  },
-
   // Add plugins
   plugins: ['shim'],
 
   // Configure shim for non-CMD modules
   shim: {
     'jquery': {
+      src: 'lib/jquery-1.9.1.min.js',
       exports: 'jQuery'
     }
   }
