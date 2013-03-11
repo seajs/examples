0a49c5cded7d153e7a8422e20047abdcd96da208
lifesinger <lifesinger@gmail.com>
2013-03-11 20:54:58 +0800

    Update sea.js to the latest version

--- static/hello/config.js
+++ static/hello/config.js
@@ -3,8 +3,8 @@ seajs.config({
   // Enable plugins
   plugins: ['shim'],
 
-  // Configure shim for non-CMD modules
-  shim: {
+  // Configure alias
+  alias: {
     'jquery': {
       src: 'lib/jquery-1.9.1.min.js',
       exports: 'jQuery'
--- static/lucky/config.js
+++ static/lucky/config.js
@@ -3,12 +3,13 @@ seajs.config({
   // Enable plugins
   plugins: ['shim'],
 
-  // Configure shim for non-CMD modules
-  shim: {
+  // Configure alias
+  alias: {
     'jquery': {
       src: 'lib/jquery-1.9.1.min.js',
       exports: 'jQuery'
     },
+
     'jquery.easing': {
       src: 'lib/jquery.easing.1.3.js',
       deps: ['jquery']
